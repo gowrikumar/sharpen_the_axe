47bcffd547196907d8de989b1cddb7a80c5463dc
gowrikumar <gowrikumar@outlook.com>
2016-09-05 19:14:27 -0700

    Quick Find and Union implementations from "Algorithms in C" by sedgewick.

---
+++ c/union_find/quickunionhalfpath.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#define N 1000
+
+int main()
+{
+	int i,j,p,q,t,id[N];
+
+	for(i=0;i<N;i++)
+		id[i] = i;
+
+	while(scanf("%d %d\n",&p,&q) == 2)
+	{
+		for(i=p;i != id[i];i=id[i])
+			id[i] = id[id[i]];
+		for(j=q;j != id[j];j=id[j])
+			id[j] = id[id[j]];
+		if(i == j)
+			continue;
+		id[i] = j;
+		printf("%d %d\n",p,q);
+	}
+	return 0;
+}
+
+
